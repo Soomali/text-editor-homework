491c78a71551e9d77fe3d0ff1afcb0ab6ad26fb6
Soomali <mehmetaliguunaydin@gmail.com>
2022-12-24 17:25:04 +0300

    chagnes.

--- src/main/java/com/example/demo/EditorApplication.java
+++ src/main/java/com/example/demo/EditorApplication.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.editor.EditorCareTaker;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
 
-public class EditorApplication extends Application {
+public class EditorApplication extends Application implements Observer {
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
+        //EditorCareTaker.getInstance().
         FXMLLoader fxmlLoader = new FXMLLoader(EditorApplication.class.getResource("editor-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
@@ -21,5 +25,11 @@ public class EditorApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+
     }
 }

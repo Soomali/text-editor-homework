71e0f45062e7b5c6a2bfde1045a1183f07c1968a
OnurArslan0 <onursln11@gmail.com>
2022-12-24 16:23:28 +0300

    mantıklı bişiler

---
+++ src/main/java/com/example/demo/setting/Setting.java
@@ -0,0 +1,9 @@
+package com.example.demo.setting;
+
+import javafx.fxml.JavaFXBuilderFactory;
+
+public interface Setting {
+    public void Save();
+
+    public void Apply();
+}
---
+++ src/main/java/com/example/demo/setting/TextSetting.java
@@ -0,0 +1,18 @@
+package com.example.demo.setting;
+
+import javafx.scene.paint.Color;
+
+public class TextSetting implements Setting {
+    public int fontSize;
+    public Color fontColor;
+
+    @Override
+    public void Save() {
+
+    }
+
+    @Override
+    public void Apply() {
+
+    }
+}
